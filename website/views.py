ff912c5af556fe83ce38fdcffca527cf2462ce3b
BautistaRalph <ralph_gabriel_bautista@dlsu.edu.ph>
2024-10-20 01:59:37 +0800

    Pivot and execution time

--- website/views.py
+++ website/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
-from .reports import get_overview, get_top, get_total, get_slice
+import time
+from .reports import get_overview, get_top, get_total, get_slice, get_game_stats
 
 views = Blueprint('views', __name__)
 
@@ -9,30 +10,47 @@ def home():
     top_results = None  
     total_results = None
     total_slice = None
+    game_stats = None  
+    execution_time = None 
 
     if request.method == 'POST':
+        start_time = time.time()
+        
+        action = request.form.get('action')
 
-        group_by_col = request.form.get('group_by_col') 
-        aggregation_col = request.form.get('aggregation_col_drill')  
-        dim_table = request.form.get('table_selection')
+        if action != 'pivot':
+            group_by_col = request.form.get('group_by_col') 
+            aggregation_col = request.form.get('aggregation_col_drill')  
+            dim_table = request.form.get('table_selection')
 
-        group_by_column1 = request.form.get('group_by_col1') 
-        aggregation_column1 = request.form.get('aggregation_col_rollup')
-        dim_table1 = request.form.get('table_selection1')
+            group_by_column1 = request.form.get('group_by_col1') 
+            aggregation_column1 = request.form.get('aggregation_col_rollup')
+            dim_table1 = request.form.get('table_selection1')
 
-        property = request.form.get('property') 
-        filter =request.form.get('filter') 
+            property = request.form.get('property') 
+            filter =request.form.get('filter') 
 
-        # Retrieve top developers or publishers
-        if group_by_col and aggregation_col and dim_table:
-            top_results = get_top(group_by_col, aggregation_col, dim_table)
+            if group_by_col and aggregation_col and dim_table:
+                top_results = get_top(group_by_col, aggregation_col, dim_table)
 
-        # Retrieve total by aggregation and group by columns
-        if group_by_column1 and aggregation_column1 and dim_table1:
-            total_results = get_total(group_by_column1, aggregation_column1, dim_table1)
+            if group_by_column1 and aggregation_column1 and dim_table1:
+                total_results = get_total(group_by_column1, aggregation_column1, dim_table1)
 
-        # Retrieve slice results
-        if property and filter:
-            total_slice = get_slice(property, filter)
+            if property and filter:
+                total_slice = get_slice(property, filter)
 
-    return render_template('base.html', overview=overview, top_results=top_results, total_results=total_results, total_slice=total_slice)
+        elif action == 'pivot':
+            game_stats = get_game_stats()  
+        
+        end_time = time.time()  
+        execution_time = end_time - start_time
+
+    return render_template(
+        'base.html', 
+        overview=overview, 
+        top_results=top_results, 
+        total_results=total_results, 
+        total_slice=total_slice,
+        game_stats=game_stats,
+        execution_time=execution_time
+    )
